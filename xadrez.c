02a96f3cffa056550ab69ec5d29cce289217f64c
welison0 <welisoncarlos123@gmail.com>
2025-08-30 00:37:10 +0000

    adicionei variavel (numeroCompleto), e adicionei a movimentação cavalo em "l" usando loop aninhado

--- xadrez.c
+++ xadrez.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int opcao, casas, i;
+    int opcao, casas, i, movimentoCompleto = 1;
 
     printf("=== Menu de Movimentos ===\n");
     printf("1 - Torre\n");
     printf("2 - Bispo\n");
     printf("3 - Rainha\n");
+    printf("4 - Cavalo\n");
     printf("Escolha a peça que deseja movimentar: ");
     scanf("%d", &opcao);
 
+    if (opcao != 4){
     printf("Digite o numero de casas que deseja mover: ");
     scanf("%d", &casas);
+    }
 
     switch(opcao) {
         case 1:
@@ -35,6 +38,15 @@ int main() {
             }
             break;
 
+        case 4:
+            printf("\n--- Movimento do Cavalo ---\n");
+            while (movimentoCompleto -- ){
+                for(i = 0; i < 2; i++){
+                    printf("Cima\n");
+                }
+                    printf("Direita\n");
+            }
+             break;
         default:
             printf("Opcao invalida!\n");
             break;
